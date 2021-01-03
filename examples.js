ff41a4c388e195418b4f02c8d0e9e749c328bfef
mathan <mathanrajhop@gmail.com>
2021-01-03 20:04:59 +0530

    new examples

--- examples.js
+++ examples.js
@@ -2,14 +2,16 @@
 //  Input: [1, 2, 3]
 //  Output: 6
 
-const array = [1, 2, 3];
 console.log("1. Given an array of numbers, find the sum of the array.");
-console.log("input", array);
-let result = 0;
-for (i=0; i<array.length; i++) {
-  result += array[i];
+console.log("input", [1, 2, 3]);
+
+Array.prototype.sum = function() {
+  return this.reduce((acc, curVal) => {
+    return acc + curVal;
+  }, 0);
 }
-console.log("output", result);
+
+console.log("output", [1, 2, 3].sum());
 
 //------------------------------------------------------------------------------------
 
@@ -18,34 +20,39 @@ console.log("output", result);
 //  Output: index: 1
 
 console.log("2. Given a number and an array of numbers, find the index of element specified.");
-const array_num = [1, 2, 3];
-console.log("input: 2,", array_num);
-const index = array_num.findIndex((element, index) => {
-  if (element === 2) {
-    return true
+console.log("input: 2,", [1, 2, 3]);
+
+Array.prototype.getIndexOf = function(el) {
+  for (var i = 0; i < this.length; i ++){
+    if(this[i]==el){
+      return i;
+    }
   }
-})
-console.log("Output: index: ", index);
+  return -1;
+}
+
+console.log("Output: index:", [1, 2, 3].getIndexOf(2));
 
 // ------------------------------------------------------------------------------------
 
 // 3. Given an array of numbers, find the elements which are even numbers.
 //   Input [1, 2, 3, 4]
 //   output: [2, 4]
-
-let evenInput = [];
-let evenOutput = [];
-evenInput.push(1, 2, 3, 4);
 console.log("3. Given an array of numbers, find the elements which are even numbers.");
-console.log("input",evenInput);
-for (i=0; i<evenInput.length; i++)
-{
-  if(evenInput[i] % 2 === 0)
-  {
-    evenOutput.push(evenInput[i]);
-  }
+console.log("input",[1, 2, 3, 4]);
+
+Array.prototype.getOddEven = function(even) {
+  let arrnum = [];
+  this.filter(function(el) {
+    if(el % 2 === even)
+    {
+      arrnum.push(el);
+    }
+  });
+  return arrnum;
 }
-console.log("output",evenOutput);
+console.log("Output : ", [1, 2, 3, 4].getOddEven(0));
+console.log("Output : ", [1, 2, 3, 4].getOddEven(1));
 
 // ------------------------------------------------------------------------------------
 
@@ -53,19 +60,21 @@ console.log("output",evenOutput);
 //   Input [1, 2, 3, 4, 5]
 //   output: [1 3, 5]
 
-let oddInput = [];
-let oddOutput = [];
-oddInput.push(1, 2, 3, 4, 5);
-console.log("4. Given an array of numbers, find the elements which are odd numbers.");
-console.log("input",oddInput);
-for (i=0; i<oddInput.length; i++)
-{
-  if(oddInput[i] % 2 === 1)
-  {
-    oddOutput.push(oddInput[i]);
-  }
-}
-console.log("output",oddOutput);
+// console.log("4. Given an array of numbers, find the elements which are odd numbers.");
+// console.log("input",[1, 2, 3, 4]);
+// let oddOutput = [];
+//
+// Array.prototype.getOdd = function(odd) {
+//   this.filter(function(el) {
+//     if(el % 2 === odd)
+//     {
+//       oddOutput.push(el);
+//     }
+//   });
+//   return oddOutput;
+// }
+//
+// console.log("Output : ", [1, 2, 3, 4].getOdd(1));
 
 // ------------------------------------------------------------------------------------
 
@@ -73,14 +82,16 @@ console.log("output",oddOutput);
 //   Input [1, 2, 3]
 //   output: [1, 4, 9]
 
-const num = [1, 2, 3];
 console.log("5. Given an array of numbers, multiply each number by itself.");
-console.log("input", num);
-let output = [];
-for (i=0; i<num.length; i++) {
-  output.push(num[i]*num[i]);
+Array.prototype.multiply = function() {
+  let multiply = [];
+   this.reduce((acc, curVal) => {
+    multiply.push(curVal * curVal);
+  }, 1);
+   return multiply;
 }
-console.log("output", output);
+
+console.log("output", [1, 2, 3, 4].multiply());
 
 //------------------------------------------------------------------------------------
 
@@ -88,21 +99,25 @@ console.log("output", output);
 // Input [1, 2, 3]
 // output: [2, 6, 3]
 
-var originalArr = [1, 2, 3];
 console.log("6. Given an array of numbers, multiply each number by the next number. The last number takes the first number as its next number.");
-console.log("input", originalArr);
-var newArr = [];
-for(var i = 0; i < originalArr.length; i++) {
-    if(originalArr.length > i+1)
-    {
-      newArr.push(originalArr[i] * originalArr[i+1]);
-    }
-    else
-    {
-      newArr.push(originalArr[i] * originalArr[0]);
-    }
+console.log("input",[1, 2, 3, 4, 5]);
+
+Array.prototype.nextMultiply = function() {
+  let multiply = [];
+  this.map((values, keys) => {
+    if(this[keys+1] === undefined)
+      {
+        multiply.push(values * this[0]);
+      }
+      else
+      {
+        multiply.push(values * this[keys+1]);
+      }
+  });
+  return multiply;
 }
-console.log("output", newArr);
+
+console.log("output", [1, 2, 3, 4, 5].nextMultiply());
 
 //------------------------------------------------------------------------------------
 
@@ -110,72 +125,72 @@ console.log("output", newArr);
 //   eg: First time i call `add(1,2)` calculates `1 + 2` and returns 3.
 //       On calling again with same inputs `add(1,2)`, it should return 3 from memory instead of calculation.
 
-console.log("7. Write a function that takes in two numbers and return its sum. Modify this function to remember the input and output. So the next time, i send in the same inputs, the result should be returned from the memory instead of calculation.");
-let QArray = [];
-function add(a, b)
-{
-  return a+b;
-}
+// console.log("7. Write a function that takes in two numbers and return its sum. Modify this function to remember the input and output. So the next time, i send in the same inputs, the result should be returned from the memory instead of calculation.");
+// let QArray = [];
+// function add(a, b)
+// {
+//   return a+b;
+// }
+//
+// let inputss = [
+//     {
+//       ins1 : 1,
+//       ins2 : 2,
+//     },
+//     {
+//       ins1 : 2,
+//       ins2 : 3,
+//     },
+//     {
+//       ins1 : 1,
+//       ins2 : 2,
+//     },
+//     {
+//       ins1 : 3,
+//       ins2 : 3,
+//     },
+//     {
+//       ins1 : 3,
+//       ins2 : 3,
+//     },
+//     {
+//       ins1 : 1,
+//       ins2 : 2,
+//     },
+//     {
+//       ins1 : 1,
+//       ins2 : 2,
+//     },
+//   ];
+//
+// Array.prototype.getIndexOf = function(el) {
+//   var arr = this;
+//   for (var i=0; i<arr.length; i++){
+//      //console.log(arr[i].input);
+//      if(arr[i].input==el){
+//        return i;
+//      }
+//   }
+//   return -1;
+// }
 
-let inputss = [
-    {
-      ins1 : 1,
-      ins2 : 2,
-    },
-    {
-      ins1 : 2,
-      ins2 : 3,
-    },
-    {
-      ins1 : 1,
-      ins2 : 2,
-    },
-    {
-      ins1 : 3,
-      ins2 : 3,
-    },
-    {
-      ins1 : 3,
-      ins2 : 3,
-    },
-    {
-      ins1 : 1,
-      ins2 : 2,
-    },
-    {
-      ins1 : 1,
-      ins2 : 2,
-    },
-  ];
-
-Array.prototype.getIndexOf = function(el) {
-  var arr = this;
-  for (var i=0; i<arr.length; i++){
-     //console.log(arr[i].input);
-     if(arr[i].input==el){
-       return i;
-     }
-  }
-  return -1;
-}
-
-inputss.map((vals, key) => {
-  //console.log(vals.ins);
-  let check = QArray.getIndexOf(vals.ins1+', '+vals.ins2);
-  if(check <= -1)
-  {
-    let adds = add(vals.ins1, vals.ins2);
-    QArray.push({
-      "input": vals.ins1+', '+vals.ins2,
-      "output": adds
-    });
-    console.log('output', adds);
-  }
-  else
-  {
-    console.log('output', QArray[check].output);
-  }
-});
+// inputss.map((vals, key) => {
+//   //console.log(vals.ins);
+//   let check = QArray.getIndexOf(vals.ins1+', '+vals.ins2);
+//   if(check <= -1)
+//   {
+//     let adds = add(vals.ins1, vals.ins2);
+//     QArray.push({
+//       "input": vals.ins1+', '+vals.ins2,
+//       "output": adds
+//     });
+//     console.log('output', adds);
+//   }
+//   else
+//   {
+//     console.log('output', QArray[check].output);
+//   }
+// });
 
 //console.log(QArray);
 
@@ -194,8 +209,8 @@ inputss.map((vals, key) => {
 //   console.log(time);
 // };
 
-function sayHi() {
-  console.log('Hello');
-}
-
-setTimeout(sayHi, 1000);
+// function sayHi() {
+//   console.log('Hello');
+// }
+//
+// setTimeout(sayHi, 1000);
